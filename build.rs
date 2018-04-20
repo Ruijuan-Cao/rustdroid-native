661afc4e4c80f16f737632f36e0e03341cb06782
Christopher Augustus <repos@c4augustus.com>
2018-04-20 10:45:08 -0700

    refactor build.rs before adding more NDK search logic

--- build.rs
+++ build.rs
@@ -1,21 +1,51 @@
-use std::path::Path;
+use std::path::PathBuf;
 use std::process::Command;
 
-fn main() {
-    // find the NDK
-    let whichndk = match Command::new("sh").arg("-c")
-            .arg("which ndk-build").output() {
-        Ok(o) => String::from_utf8(o.stdout).unwrap_or(String::new()),
+fn find_which_ndk_build_path() -> PathBuf {
+    let mut ndkpath = PathBuf::new();
+    let mut cmd = Command::new("sh");
+    cmd.arg("-c").arg("which ndk-build");
+    match cmd.output() {
         Err(e) => {
-            println!("cargo:warning=ERROR executing command: {}", e);
-            String::new()
+            println!("cargo:warning=Error executing process command <{:?}>: {}",
+                cmd, e);
+        },
+        Ok(o) => {
+            match String::from_utf8(o.stdout) {
+                Err(e) => {
+                    println!("cargo:warning=Error parsing path string: {}",
+                        e);
+                },
+                Ok(s) => {
+                    ndkpath = PathBuf::from(&s);
+                    ndkpath.pop();
+                },
+            }
         },
     };
-    if ! whichndk.is_empty() {
-        let pathndk = Path::new(&whichndk).parent().unwrap_or(Path::new(""));
-        println!("cargo:warning=NDK found at path {}",
-            pathndk.to_string_lossy());
-    } else {
-        println!("cargo:warning=NDK not found");
-    }
+    ndkpath
+}
+
+fn find_ndk_path() -> PathBuf {
+    find_which_ndk_build_path()
+}
+
+fn show_ndk_path(ndkpath: PathBuf) {
+    let ndkpathstr = ndkpath.to_string_lossy();
+    println!("cargo:warning=NDK path {}{}",
+        if ndkpathstr.is_empty() { "not found" } else { "found at " },
+        ndkpathstr);
+}
+
+fn establish_ndk() {
+    let ndkpath = find_ndk_path();
+    show_ndk_path(ndkpath);
+}
+
+fn establish_ndk_toolchain() {
+    establish_ndk();
+}
+
+fn main() {
+    establish_ndk_toolchain();
 }
