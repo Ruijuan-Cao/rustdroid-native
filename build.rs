19baeabe80c3ab4f9daf947d2cfcdfd77635cfbf
Christopher Augustus <repos@c4augustus.com>
2018-04-25 22:42:52 -0700

    continue build.rs NDK path finding using environment vars and known locations

--- build.rs
+++ build.rs
@@ -1,7 +1,8 @@
+use std::env::var;
 use std::path::PathBuf;
 use std::process::Command;
 
-fn find_which_ndk_build_path() -> Option<PathBuf> {
+fn command_which_ndk_build_path() -> Option<PathBuf> {
     let mut cmd = Command::new("sh"); // mut due to API limitation
     cmd.arg("-c").arg("which ndk-build");
     match cmd.output() {
@@ -24,21 +25,58 @@ fn find_which_ndk_build_path() -> Option<PathBuf> {
     }
 }
 
-fn find_environment_ndk_path() -> Option<PathBuf> {
-    // TODO: ### IMPLEMENT
-    None
+fn find_ndk_env_var_string() -> Option<String> {
+    var("ANDROID_NDK_HOME")
+        .or_else(|_| var("NDK_HOME")
+        .or_else(|_| var("NDK_ROOT")
+        .or_else(|_| var("NDKROOT"))))
+        .ok()
+}
+
+fn find_sdk_env_var_string() -> Option<String> {
+    var("ANDROID_SDK_HOME")
+        .or_else(|_| var("ANDROID_HOME"))
+        .ok()
+}
+
+fn find_env_ndk_path() -> Option<PathBuf> {
+    match find_ndk_env_var_string().or_else(
+        || find_sdk_env_var_string().map(|s| s + "ndk-bundle")) {
+        Some(s) => Some(PathBuf::from(s)),
+        None => None,
+    }
 }
 
 fn find_known_ndk_path() -> Option<PathBuf> {
-    // TODO: ### IMPLEMENT
-    Some(PathBuf::from("~/Library/Android/sdk/ndk-bundle"))
-        // ^ TODO: ### FOR TESTING
+    // TODO: @@@ MAKE THIS RUSTY
+    let pathandroidstudio = PathBuf::from("~/Library/Android/sdk/ndk-bundle");
+    if pathandroidstudio.exists() {
+        Some(pathandroidstudio)
+    } else {
+        let pathnvidiacodeworks = PathBuf::from("~/NVPACK");
+        if pathnvidiacodeworks.exists() {
+            Some(pathnvidiacodeworks)
+        } else {
+            None
+        }
+    }
+}
+
+fn find_ndk_build_path() -> Option<PathBuf> {
+    find_env_ndk_path().or_else(
+        || find_known_ndk_path().and_then(
+            |p| if p.join("bin").join("ndk-build").exists() {
+                Some(p)
+            } else {
+                None
+            }
+        )
+    )
 }
 
 fn find_ndk_path() -> Option<PathBuf> {
-    find_which_ndk_build_path()
-        .or_else(|| find_environment_ndk_path())
-        .or_else(|| find_known_ndk_path())
+    command_which_ndk_build_path()
+        .or_else(|| find_ndk_build_path())
 }
 
 fn establish_ndk() {
